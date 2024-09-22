739073d53f254fc1644982d4e126ba54d6a6c77a
pranaaavj <jpvelloor@gmail.com>
2024-09-22 21:23:10 +0530

    finished bug

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -1,4 +1,4 @@
-import { createApi } from '@reduxjs/toolkit/query';
+import { createApi } from '@reduxjs/toolkit/query/react';
 import { baseQueryWithReAuth } from './baseApi';
 
 export const authApi = createApi({
